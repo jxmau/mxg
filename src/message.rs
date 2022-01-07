cc7a2b554dbc821e35145d775ffa90676a098f6f
jxmau <jxmau.pro@outlook.fr>
2022-01-07 17:34:36 +0100

    Update on the DebugMessage struct * Fields: public to private * Adding highlight method * Redone how the line and col are displayed

--- src/message.rs
+++ src/message.rs
@@ -26,26 +26,34 @@ impl Kind {
 }
 
 pub struct DebugMessage{
-    pub kind: Kind,
-    pub title: String,
-    pub line: String,
+    kind: Kind,
+    title: String,
+    line: String,
     subline: String,
-    pub line_nb: u8,
-    pub explain: String,
+    explain: String,
 }
 
 impl DebugMessage {
 
 
     /// Return a new, empty Debug Message struct.
+    /// This will create the core of the message. To specify the location of the error, use the locate method.
     pub fn new(kind: Kind, title: &str, line_nb: u8) -> Self {
         let line = format!("l.{} | ", line_nb);
         let mut subline = String::new();
         subline.add_patt(" ", line.len());
-        DebugMessage {kind: kind.clone(), title: kind.get_title(title).to_string(), line, line_nb, explain: String::new(), subline }
+        DebugMessage {kind: kind.clone(), title: kind.get_title(&title).to_string(), line, explain: String::new(), subline }
     }
 
-    /// Will appnd the line.
+    /// Will create a prefix for the title with the name of the file, the line and the column where the error is located and/or starts.
+    pub fn locate(&mut self, name: &str, line: u8, col: u8) -> &mut Self {
+        let mut prefix = format!("{} {} - {} | ", name, line, col);
+        prefix.push_str(&self.title);
+        self.title = self.kind.get_title(&prefix);  
+        self
+    }
+
+    /// Will append the line.
     /// This will create the field where the query debugged is displayed.
     pub fn append(&mut self, line: &str) -> &mut Self{
         self.line.push_str(line);
@@ -67,16 +75,18 @@ impl DebugMessage {
         self
     }
 
-    /// Append an explanation displayed after the query.
-    pub fn explain(&mut self, explain: &str) -> &mut Self{
-        self.explain = explain.into();
+    /// To be used when pointing to a slice of a query when no suggestion are needed. 
+    pub fn highlight(&mut self, hl: &str) -> &mut Self {
+        self.line.push_str(&Yellow.paint(hl).to_string());
+        self.subline.add_patt(&Yellow.paint("^").to_string(), hl.len());
         self
     }
 
-    /// Will print the message.
-    pub fn consume(&self) {
-        println!("\n{}\n\n    {}\n    {}\n\n   {}\n", self.title, self.line, self.subline, self.explain);
-    } 
+    /// Append an explanation displayed after the query.
+    pub fn explain(&mut self, explain: &str) -> &mut Self{
+        self.explain.push_str(&format!("{}\n", explain));
+        self
+    }
 
 }
 
